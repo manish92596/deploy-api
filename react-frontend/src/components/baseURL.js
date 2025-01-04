f46ef00687672a019b99af6d8d16bfc3d11b4f85
manish92596 <manishkumar92596@gmail.com>
2025-01-04 14:29:09 +0530

    new baseurl

--- react-frontend/src/components/baseURL.js
+++ react-frontend/src/components/baseURL.js
@@ -1,2 +1,3 @@
-const BASE_URL = "https://deploy-api-49dn.onrender.com/";
+// const BASE_URL = "https://deploy-api-49dn.onrender.com/";
+const BASE_URL = "http://43.204.234.122/";
 export default BASE_URL;
